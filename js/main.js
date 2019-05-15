1f6289874dcc5dabd7bc922d101bccee08b0d5ce
rogeruwayezu <rogeruwayezu@gmail.com>
2019-05-15 10:56:43 +0200

    changes

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import Parallax from 'parallax-js'
+// $('.parallax-window').parallax({ imageSrc: '/path/to/image.jpg' });
+var header = document.getElementById('header');
+var parallaxInstance = new Parallax(header);
+
+var about = document.getElementById('about');
+var parallaxInstance = new Parallax(about);
