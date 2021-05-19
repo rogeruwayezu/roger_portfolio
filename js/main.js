13148ad52faecd276ecc91d26b7e86bdfbb368ad
rogeruwayezu <rogeruwayezu@gmail.com>
2021-05-19 09:41:28 +0200

    added new changes

--- js/main.js
+++
@@ -1,7 +0,0 @@
-import Parallax from 'parallax-js'
-// $('.parallax-window').parallax({ imageSrc: '/path/to/image.jpg' });
-var header = document.getElementById('header');
-var parallaxInstance = new Parallax(header);
-
-var about = document.getElementById('about');
-var parallaxInstance = new Parallax(about);
